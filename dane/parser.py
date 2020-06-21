1ecb23afee6dab36a73716d135dc963f9266435c
szymsid <szy.sidoruk@gmail.com>
2020-06-21 14:06:55 +0200

    ready data

--- dane/parser.py
+++ dane/parser.py
@@ -1,6 +1,8 @@
 import json
 import time
 import copy
+import random
+
 from datetime import datetime, timedelta
 
 with open("deliveries.jsonl","r") as d:
@@ -12,11 +14,17 @@ with open("sessions.jsonl","r") as s:
 with open("users.jsonl","r") as u:
     users = u.read().splitlines()
 
-result = {}
+result = {
+    360: {},
+    516: {},
+    620: {},
+}
+uid_city_map = {}
 
 for user in users:
     user = json.loads(user)
-    result[user["user_id"]] = {"city": user["city"], "deliveries": []}
+    uid_city_map[user["user_id"]] = user["city"]
+    #result[user["user_id"]] = {"city": user["city"], "deliveries": []}
 
 prev_session = None
 last_info = []
@@ -24,12 +32,13 @@ last_info = []
 for session in sessions:
     session = json.loads(session)
     if len(last_info) > 0:
-        print(last_info)
         if session["session_id"] == prev_session:
             if session["user_id"] is not None:
-                print(f"u: {session['user_id']} s: {session['session_id']}")
                 for info in last_info:
-                    result[session["user_id"]]["deliveries"].append(info)
+                    if uid_city_map[session["user_id"]] in result[info[0]].keys():
+                        result[info[0]][uid_city_map[session["user_id"]]].append(info[1])
+                    else:
+                        result[info[0]][uid_city_map[session["user_id"]]] = [info[1]]
                 last_info.clear()
         else:
             last_info.clear()
@@ -37,6 +46,7 @@ for session in sessions:
         time_sent = None
         time_delivery = None
         info = {}
+        delivery_company = None
         for delivery in deliveries:
             delivery = json.loads(delivery)
             if delivery["purchase_id"] == session["purchase_id"]:
@@ -45,32 +55,62 @@ for session in sessions:
                 if delivery["delivery_timestamp"] is not None:
                     time_delivery = datetime.strptime(delivery["delivery_timestamp"],'%Y-%m-%dT%H:%M:%S.%f')
                 if delivery["delivery_company"] is not None:
-                    info = {"delivery_company": delivery["delivery_company"]}
+                    delivery_company = delivery["delivery_company"]
                 break
         
-        if time_sent is not None and time_delivery is not None and "delivery_company" in info.keys():
+        if time_sent is not None and time_delivery is not None and delivery_company is not None:
             delta_time = (time_delivery - time_sent) / timedelta(days=1)
             if delta_time > 0:
                 info["delta_time"] = delta_time
                 info["shipment_day"] = time_sent.weekday()
                 if session["user_id"] is not None:
-                    result[session["user_id"]]["deliveries"].append(info)
+                    if uid_city_map[session["user_id"]] in result[delivery_company].keys():
+                        result[delivery_company][uid_city_map[session["user_id"]]].append(info)
+                    else:
+                        result[delivery_company][uid_city_map[session["user_id"]]] = [info]
                 else:
-                    last_info.append(info)
+                    last_info.append((delivery_company, info))
             
     prev_session = session["session_id"]
 
-full_result = {}
-for res in result.keys():
-    if result[res]["city"] in full_result.keys():
-        full_result[result[res]["city"]] += result[res]["deliveries"]
-    else:
-        full_result[result[res]["city"]] = copy.deepcopy(result[res]["deliveries"])
 
+random.seed(0)
 
-with open("data.jsonl", "w") as out:
-    for city in full_result.keys():
-        out.write("{\"")
-        out.write(f"{city}\": ")
-        out.write(json.dumps(full_result[city]))
-        out.write("}\n")
+for delivery_company in result.keys():
+    training = []
+    validation = []
+    testing = []
+    for city in result[delivery_company].keys():
+        training_size = int(0.6 * len(result[delivery_company][city]))
+        for i in range(training_size):
+            index = random.randint(0,len(result[delivery_company][city])-1)
+            delivery = result[delivery_company][city].pop(index)
+            delivery["city"] = city
+            training.append(delivery)
+        validation_size = int(0.5 * len(result[delivery_company][city]))
+        for i in range(validation_size):
+            index = random.randint(0,len(result[delivery_company][city])-1)
+            delivery = result[delivery_company][city].pop(index)
+            delivery["city"] = city
+            validation.append(delivery)
+        for delivery in result[delivery_company][city]:
+            delivery["city"] = city
+            testing.append(delivery)
+
+    filename = (f"{delivery_company}_training.jsonl")
+    with open(filename, "w") as out:
+        for delivery in training:
+            out.write(json.dumps(delivery))
+            out.write("\n")
+
+    filename = (f"{delivery_company}_validation.jsonl")
+    with open(filename, "w") as out:
+        for delivery in validation:
+            out.write(json.dumps(delivery))
+            out.write("\n")
+    
+    filename = (f"{delivery_company}_testing.jsonl")
+    with open(filename, "w") as out:
+        for delivery in testing:
+            out.write(json.dumps(delivery))
+            out.write("\n")
